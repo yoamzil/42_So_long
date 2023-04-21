7bf63d87d64b73936a0d3a7008fc33003deeef4b
yoamzil <amzilyouness@gmail.com>
2023-04-21 22:35:19 +0000

    Added map checkers

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: yoamzil <yoamzil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 01:23:57 by yoamzil           #+#    #+#             */
-/*   Updated: 2023/04/21 20:03:33 by yoamzil          ###   ########.fr       */
+/*   Updated: 2023/04/21 22:34:43 by yoamzil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	**read_map(char *filepath)
 
 	fd = open(filepath, O_RDONLY);
 	if (fd == -1)
-		return (NULL);
+		return (NULL); 
 	accumulator = ft_strdup("");
 	while (1)
 	{
@@ -40,26 +40,149 @@ char	**read_map(char *filepath)
 	return (map_array);
 }
 
+int is_rectangular(char **map)
+{
+    size_t	row = 0;
+    size_t	width = 0;
+
+    if (map == NULL)
+        return (0);
+
+    while (map[row] != NULL)
+    {
+        if (width == 0)
+            width = ft_strlen(map[row]);
+        else if (width != ft_strlen(map[row]))
+            return (0);
+        row++;
+    }
+
+    return (1);
+}
+
+int is_surrounded_by_walls(char **map)
+{
+    int i, j;
+    // Check top and bottom walls
+    i = 0;
+    while (map[i])
+    {
+        if (map[i][0] != '1' || map[i][ft_strlen(map[i]) - 1] != '1')
+            return (0);
+        i++;
+    }
+    // Check left and right walls
+    i = 1;
+    while (map[i])
+    {
+        j = 0;
+        while (map[i][j])
+        {
+            if ((j == 0 || j == (int)ft_strlen(map[i]) - 1) && map[i][j] != '1')
+                return (0);
+            j++;
+        }
+        i++;
+    }
+    return (1);
+}
+
+int has_valid_pec(t_game *game)
+{
+    int i = 0;
+    int j = 0;
+
+    game->num_collect = 0;
+    game->num_player = 0;
+    game->num_exit = 0;
+
+    while (game->map[i])
+    {
+        j = 0;
+        while (game->map[i][j])
+        {
+            if (game->map[i][j] == 'P')
+                game->num_player++;
+            else if (game->map[i][j] == 'E')
+                game->num_exit++;
+            else if (game->map[i][j] == 'C')
+                game->num_collect++;
+            j++;
+        }
+        i++;
+    }
+    if (game->num_player != 1 || game->num_exit == 0 || game->num_collect == 0)
+        return (0);
+
+    return (1);
+}
+
+int is_valid_map_chars(char **map)
+{
+    int i = 0;
+    int j;
+    while (map[i])
+	{
+        j = 0;
+        while (map[i][j]) {
+            char c = map[i][j];
+            if (c != 'P' && c != 'E' && c != 'C' && c != '0' && c != '1') {
+                return 0;
+            }
+            j++;
+        }
+        i++;
+    }
+    return 1;
+}
+
+int is_valid_map(t_game *game)
+{
+    if (is_rectangular(game->map) && is_surrounded_by_walls(game->map)
+        && has_valid_pec(game) && is_valid_map_chars(game->map))
+        return (1);
+    return (0);
+}
+
+int	is_valid_arg(char *arg)
+{
+	int	len;
+
+	if (!arg)
+		return (0);
+
+	len = 0;
+	while (arg[len] != '\0')
+		len++;
+	if (arg[len - 1] == 'r' && arg[len - 2] == 'e' && arg[len - 3] == 'b' && arg[len - 4] == '.')
+	{
+		return (1);
+	}
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
-	t_map	map;
+	t_game	game;
 
 	if (argc == 2)
 	{
-		map.array = read_map(argv[1]);
-		// if (is_valid_map(&map) && is_valid_arg(argv[1]))
-		// {
+		game.array = read_map(argv[1]);
+		printf("wselt hna\n");
+		if (is_valid_map(&game) && is_valid_arg(argv[1]))
+		{
+			printf("Valid Map");
 		// 	init_game(&map);
 		// 	start_gameplay(&map);
 		// 	mlx_loop(map.mlx);
-		// }
-		// else
-		// {
-		// 	if (map.array)
-		// 		free_map(map.array);
-		// 	printf("Invalid Map");
-		// 	exit(1);
-		// }
+		}
+		else
+		{
+			if (game.array)
+				// free_map(game.array);
+				printf("Invalid Map");
+			exit(1);
+		}
 	}
 	else
 	{
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: yoamzil <yoamzil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 01:24:06 by yoamzil           #+#    #+#             */
-/*   Updated: 2023/04/21 19:49:27 by yoamzil          ###   ########.fr       */
+/*   Updated: 2023/04/21 22:23:25 by yoamzil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,22 @@
 # include "libft/libft.h"
 # include "get_next_line/get_next_line.h"
 
-typedef struct	s_map
+typedef struct	s_game
 {
+    char    **map;
     int     width;
     int     height;
     char    **array;
 	void	*mlx;
-}	t_map;
+    int     num_player;
+    int     num_collect;
+    int     num_exit;
+}	t_game;
 
 char	**read_map(char *filepath);
+int     is_valid_map(t_game *map);
+int     is_rectangular(char **map);
+int     is_valid_arg(char *arg);
+int     is_surrounded_by_walls(char **map);
 
 #endif
